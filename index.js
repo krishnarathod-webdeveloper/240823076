d23b06fdc94e0f91b31930c80135b0ba3ac70f76
krishnarathod-webdeveloper <krishnarathod.webdev@gmail.com>
2025-07-15 11:39:25 +0530

    sum of natural numbers

--- index.js
+++ index.js
@@ -7,3 +7,31 @@
 // {
 //     console.log("odd number")
 // }
+
+// let a=5;
+// let b=9;
+// if(a>b)
+// {
+//     console.log("a is larger " +a);
+// }
+// else{
+//     console.log("b is larger " +b);
+// }
+
+// let year=2020;
+// if(year%4==0)
+// {
+//     console.log("leap year");
+// }
+// else
+// {
+//     console.log("not leap year");
+// }
+
+let sum =0;
+for(let i=1; i<=5; i++ )
+{
+    sum=sum+i
+}
+console.log(sum)
+
